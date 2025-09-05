4ec2d0b9c31ca2bb662967a3a964d2faa5e371df
saran0602A <sarankumar0602a@gmail.com>
2025-09-05 12:47:14 +0530

    done with something

--- app/Home/page.tsx
+++ app/Home/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/header'
 import Hero from '../components/hero'
+import About from '../components/about'
 
 export default function page() {
   return (
 <div className="w-full min-h-screen overflow-x-hidden no-scrollbar">
   <Header />
   <Hero />
+  <About />
   <section className="min-h-screen"></section>
   <section className="min-h-screen"></section>
 </div>
